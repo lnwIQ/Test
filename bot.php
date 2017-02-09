2de2ddec512c8450d03260d47ad25b3905a74f1d
lnwIQ <van_king@windowslive.com>
2017-02-09 07:55:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				'type' => "บอท",
+				'text' => "ว่าไงครับท่าน"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
